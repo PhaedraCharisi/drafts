ffd95a385d108efc228aafaddf57d9dc74bddb00
Phaedra <103902696+PhaedraCharisi@users.noreply.github.com>
2025-08-12 19:03:36 +0300

    Add files via upload

--- demo4.m
+++ demo4.m
@@ -14,3 +14,25 @@ size(freqs)
 h = wbispec_entropy(wbis);
 
 s = total_sumwbic(sumwbic);
+
+%%
+
+data = readmatrix('C:\Users\xaris\Documents\pads-parkinsons-disease-smartwatch-dataset-1.0.0\movement\timeseries\060_HoldWeight_RightWrist.txt', 'Delimiter', ',');
+data = data(50:end,:);
+
+signal = data(:,2);
+[wbis,wbic, sumwbic, freqs] = wbispecV2(signal,100,0,1); % (1->true, 0->false)
+
+[a,b,c,d] = extract_prominent_wb_peaks(wbis);
+h = wbispec_entropy(wbis);
+h
+
+[e,f,g,l] = extract_prominent_wb_peaks(wbic);
+m = wbispec_entropy(wbic);
+m
+s = total_wbic(sumwbic);
+s
+
+[a,b,c,d] = extract_prominent_wb_peaksV2(wbis,1);
+
+metrics = wb_pair_metrics(wbis) % [pairCount, pairFraction, pairEnergyShare, meanDiagDistance]
